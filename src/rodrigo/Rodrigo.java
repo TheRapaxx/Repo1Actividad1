5b0bbe9648c5a235513670036e70f763cd9fef6e
Tu Nombre <tu.email@example.com>
2024-05-09 18:54:19 +0200

    Añade clase Rodrigo con método de saludo

---
+++ src/rodrigo/Rodrigo.java
@@ -0,0 +1,11 @@
+package rodrigo;
+
+public class Rodrigo {
+    public static void main(String[] args) {
+        saludo();  // Llamada al método que imprime el mensaje
+    }
+
+    public static void saludo() {
+        System.out.println("Hola, soy Rodrigo");
+    }
+}
